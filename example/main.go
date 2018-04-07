f7cf531711ef59ffc167b2a851cf7ef474ae19a5
Mr. Tseng <jiahonzheng@gmail.com>
2018-04-07 22:52:04 +0800

    各功能基本成熟

--- example/main.go
+++ example/main.go
@@ -1,34 +1,86 @@
 package main
 
 import (
-	"log"
+	"fmt"
 	"../greatjson"
 )
 
 func main() {
 	data := []byte(`{
-		"test": {
-			"array": [1, "2", 3],
-			"arraywithsubs": [
-				{"subkeyone": 1},
-				{"subkeytwo": 2, "subkeythree": 3}
-			],
-			"bignum": 8000000000
+		"test_data": {
+			"int": 1,
+			"float": 1.023,
+			"string": "Hello World",
+			"bool": true,
+			"array": [1, 2.333, "Hello World", true, null],
+			"array_with_object": [
+				{"key_one": 1},
+				{"key_one": 1, "key_two": 2}
+			]
 		}
 	}`)
 
-	js, err := greatjson.New(data)
+	js, err := greatjson.NewFrom(data)
 
 	if err != nil {
-		log.Println(err)
+		fmt.Println(err)
 	}
 
-	log.Println(js.Get("test").Get("bignum"))
+	// 获取转换后的 json 实例
+	fmt.Println("获取转换后的 json 实例")
+	fmt.Println(js.Map())
+	fmt.Println()
 
-	js, err = greatjson.New([]byte{})
+	// 检查指定 key 的 value 存在性
+	fmt.Println("检查指定 key 的 value 存在性")
+	value, ok := js.CheckGet("test_existence")
+	fmt.Println(value, ok)
+	fmt.Println()
+
+	// 获取 int
+	fmt.Println("获取 int")
+	fmt.Println(js.Get("test_data").Get("int").Int())
+	fmt.Println()
+
+	// 获取 float32
+	fmt.Println("获取 float32")
+	fmt.Println(js.Get("test_data").Get("float").Float32())
+	fmt.Println()
+
+	// 获取 float64
+	fmt.Println("获取 float64")
+	fmt.Println(js.Get("test_data").Get("float").Float64())
+	fmt.Println()
+
+	// 获取 string
+	fmt.Println("获取 string")
+	fmt.Println(js.Get("test_data").Get("string").String())
+	fmt.Println()
+
+	// 获取 bool
+	fmt.Println("获取 bool")
+	fmt.Println(js.Get("test_data").Get("bool").Bool())
+	fmt.Println()
+
+	// 获取 array
+	fmt.Println("获取 array")
+	fmt.Println(js.Get("test_data").Get("array").Array())
+	fmt.Println()
+
+	// 获取 array 里的指定 index 的值
+	fmt.Println("获取 array 里的指定 index 的值")
+	fmt.Println(js.Get("test_data").Get("array").GetIndex(0).Int())
+	fmt.Println(js.Get("test_data").Get("array").GetIndex(1).Float32())
+	fmt.Println(js.Get("test_data").Get("array").GetIndex(2).String())
+	fmt.Println(js.Get("test_data").Get("array").GetIndex(3).Bool())
+	fmt.Println(js.Get("test_data").Get("array").GetIndex(4).Data())
+	fmt.Println(js.Get("test_data").Get("array_with_object").GetIndex(1).Get("key_one").Int())
+	fmt.Println()
+
+	js, err = greatjson.New()
 
 	if err != nil {
-		log.Println(err)
+		fmt.Println(err)
 	}
 
 	js.Set("a", 1)
@@ -36,8 +88,8 @@ func main() {
 	data, err = js.Marshal()
 
 	if err != nil {
-		log.Println(err)
+		fmt.Println(err)
 	}
 
-	log.Println(string(data))
+	fmt.Println(string(data))
 }
--- greatjson/greatjson.go
+++ greatjson/greatjson.go
@@ -3,6 +3,7 @@ package greatjson
 import (
 	"encoding/json"
 	"errors"
+	"reflect"
 )
 
 // JSON 定义
@@ -10,13 +11,22 @@ type JSON struct {
 	data interface{}
 }
 
-// New 解析 data 并生成 JSON 结构体指针
-func New(data []byte) (*JSON, error) {
+// New 生成空 JSON
+func New() (*JSON, error) {
 	j := new(JSON)
 
-	if len(data) == 0 {
-		data = []byte(`{}`)
+	err := j.Unmarshal([]byte(`{}`))
+
+	if err != nil {
+		return nil, err
 	}
+
+	return j, nil
+}
+
+// NewFrom 解析 data 并生成 JSON 结构体指针
+func NewFrom(data []byte) (*JSON, error) {
+	j := new(JSON)
 
 	err := j.Unmarshal(data)
 
@@ -37,6 +47,19 @@ func (j *JSON) Unmarshal(data []byte) error {
 	return json.Unmarshal(data, &j.data)
 }
 
+// CheckGet 检查 key 存在性
+func (j *JSON) CheckGet(key string) (*JSON, bool) {
+	m, err:= j.Map()
+
+	if err == nil {
+		if value, ok := m[key]; ok {
+			return &JSON{value}, true
+		}
+	}
+
+	return nil, false
+}
+
 // Get 获取 key 对应的 value
 func (j *JSON) Get(key string) *JSON {
 	m, err := j.Map()
@@ -44,6 +67,19 @@ func (j *JSON) Get(key string) *JSON {
 	if err == nil {
 		if value, ok := m[key]; ok {
 			return &JSON{value}
+		}
+	}
+
+	return &JSON{nil}
+}
+
+// GetIndex 获取指定 index 的 value
+func (j *JSON) GetIndex(index int) *JSON {
+	arr, err := j.Array()
+
+	if err == nil {
+		if len(arr) > index {
+			return &JSON{arr[index]}
 		}
 	}
 
@@ -78,10 +114,96 @@ func (j *JSON) Map() (map[string]interface{}, error) {
 		return m, nil
 	}
 
-	return nil, errors.New("类型断言错误")
+	return nil, errors.New("类型断言失败")
+}
+
+// Array 转义为数组
+func (j *JSON) Array() ([]interface{}, error) {
+	if arr, ok := (j.data).([]interface{}); ok {
+		return arr, nil
+	}
+
+	return nil, errors.New("数组类型断言失败")
 }
 
 // Data 获得 JSON 的 data 段
 func (j *JSON) Data() interface{} {
 	return j.data
+}
+
+// Bool 转义为 bool
+func (j *JSON) Bool() (bool, error) {
+	if b, ok := (j.data).(bool); ok {
+		return b, nil
+	}
+
+	return false, errors.New("布尔类型断言失败")
+}
+
+func (j *JSON) String() (string, error) {
+	if str, ok := (j.data).(string); ok {
+		return str, nil
+	}
+
+	return "", errors.New("字符串类型断言失败")
+}
+
+// Int 转义为 int
+func (j *JSON) Int() (int, error) {
+	switch j.data.(type) {
+	case json.Number:
+		i, err := j.data.(json.Number).Int64()
+		return int(i), err
+	case int, int8, int16, int32, int64:
+		i := int(reflect.ValueOf(j.data).Int())
+		return i, nil
+	case uint, uint8, uint16, uint32, uint64:
+		i := int(reflect.ValueOf(j.data).Uint())
+		return i, nil
+	case float32, float64:
+		i := int(reflect.ValueOf(j.data).Float())
+		return i, nil
+	}
+
+	return 0, errors.New("数值无法转换为 int")
+}
+
+// Float32 转义为 float32
+func (j *JSON) Float32() (float32, error) {
+	switch j.data.(type) {
+	case json.Number:
+		f, err := j.data.(json.Number).Float64()
+		return float32(f), err
+	case int, int8, int16, int32, int64:
+		f := float32(reflect.ValueOf(j.data).Int())
+		return f, nil
+	case uint, uint8, uint16, uint32, uint64:
+		f := float32(reflect.ValueOf(j.data).Uint())
+		return f, nil
+	case float32, float64:
+		f := float32(reflect.ValueOf(j.data).Float())
+		return f, nil
+	}
+
+	return 0, errors.New("数值无法转换为 float32")
+}
+
+// Float64 转义为 float64
+func (j *JSON) Float64() (float64, error) {
+	switch j.data.(type) {
+	case json.Number:
+		f, err := j.data.(json.Number).Float64()
+		return f, err
+	case int, int8, int16, int32, int64:
+		f := float64(reflect.ValueOf(j.data).Int())
+		return f, nil
+	case uint, uint8, uint16, uint32, uint64:
+		f := float64(reflect.ValueOf(j.data).Uint())
+		return f, nil
+	case float32, float64:
+		f := float64(reflect.ValueOf(j.data).Float())
+		return f, nil
+	}
+
+	return 0, errors.New("数值无法转换为 float64")
 }
